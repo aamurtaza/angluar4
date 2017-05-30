b34bfe010e6fddd3adb1700322b93b648d064687
adnanbajwa <adnan.bajwa163@gmail.com>
2017-05-30 18:07:09 +0300

    Passing data to app component, two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   // template:`<h1>{{title}}</h1><p> {{description}}! cool</p>`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   description = 'some description';
+  routeSub: any;
+  query: string;
+
+  constructor(private route:ActivatedRoute) {}
+
+  ngOnInit() {
+	this.routeSub = this.route.params.subscribe(params =>{
+  		console.log(params)
+  		this.query = params['q']
+  	})
+  }
+
+  ngOnDestroy() {
+  	this.routeSub.unsubscribe()
+  }
 }
