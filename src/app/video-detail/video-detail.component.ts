572a2ebe45cb67ed57373ffa5f6e9d24f654c11b
adnanbajwa <adnan.bajwa163@gmail.com>
2017-06-02 17:11:59 +0300

    use video service in video_detail component

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -2,30 +2,28 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Http } from '@angular/http';
 
+import { VideoService } from '../videos/video.service';
+
 @Component({
   selector: 'video-detail',
   templateUrl: './video-detail.component.html',
-  styleUrls: ['./video-detail.component.css']
+  styleUrls: ['./video-detail.component.css'],
+  providers: [VideoService]
 })
 export class VideoDetailComponent implements OnInit {
 	private routeSub:any;
   private req:any;
   video: any;
-  //Default video object to avoid an error
-  // video = {
-  //     name: "Default",
-  //     slug: "Item-1",
-  //     embed: "1hyjLD7pk10"
-  // }
 	slug:String;
-  constructor(private route:ActivatedRoute, private http:Http) { }
+
+  constructor(private route:ActivatedRoute, private http:Http, private _videos:VideoService) { }
 
   ngOnInit() {
   		this.routeSub = this.route.params.subscribe(params => {
   			console.log(params)
   			this.slug = params['slug']
-        this.http.get('assets/json/videos.json').subscribe(data => {
-            data.json().filter(item => {
+        this.req = this._videos.list().subscribe(data => {
+            data.filter(item => {
                 if (item.slug == this.slug) {
                   console.log(item)
                   this.video = item
