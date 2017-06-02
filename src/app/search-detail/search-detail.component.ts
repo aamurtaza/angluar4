6c601765f6017f4071b39e320a0aab4711b2b2ec
adnanbajwa <adnan.bajwa163@gmail.com>
2017-06-02 21:54:32 +0300

    search video list by name

--- src/app/search-detail/search-detail.component.ts
+++ src/app/search-detail/search-detail.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { VideoService } from '../videos/video.service';
+
 @Component({
   selector: 'app-search-detail',
   templateUrl: './search-detail.component.html',
-  styleUrls: ['./search-detail.component.css']
+  styleUrls: ['./search-detail.component.css'],
+  providers: [VideoService],
 })
 export class SearchDetailComponent implements OnInit {
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private _videos:VideoService) { }
   	routeSub: any
+    req:any
   	query: String;
+    videoList: [any];
+
   ngOnInit() {
   	this.routeSub = this.route.params.subscribe(params =>{
   		console.log(params)
   		this.query = params['q']
+      this.req = this._videos.search(this.query).subscribe(data => {
+              this.videoList = data as any;
+      })
   	})
   }
 
   ngOnDestroy(){
   	this.routeSub.unsubscribe()
+    this.req.unsubscribe()
   }
 
+  getEmbedUrl(item){
+    return 'https://www.youtube.com/embed/' + item.embed + '?ecver=2'
+  }
 }
--- src/app/videos/video.service.ts
+++ src/app/videos/video.service.ts
@@ -35,6 +35,21 @@ export class VideoService {
 
   }
 
+  search(query) {
+  	 return this.http.get(endpoint)
+  	 .map(response=>{
+  	 				let data = []
+  	 	 			let req  = response.json().filter(item=>{
+  									if (item.name.indexOf(query) >=0) {
+  										data.push(item)
+  									}
+  	 							})
+  	 				return data;
+  	 			})
+  	 .catch(this.handleError)
+
+  }
+
   private handleError(error: any, caught: any) :any {
   	console.log(error,caught)
   }
